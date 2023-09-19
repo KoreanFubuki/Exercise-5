29b5e5444d500695f8e91c52b1e61c11bfc4a784
KoreanFubuki <145092552+KoreanFubuki@users.noreply.github.com>
2023-09-19 16:51:54 +0400

    Add project files.

---
+++ Exercise 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise5
+{
+    class Program
+    {
+        static int highscore = 500;
+        static string HighschorePlayer = "Xx__Slayer__xX";
+        static void Main(string[] args)
+        {
+            game(350, "Gamer");
+            game(499, "Random Edgelord");
+            game(696, "Fubuki");
+
+            Console.Read();
+        }
+        public static void game(int score, string playername)
+        {
+            if (score > highscore)
+            {
+                highscore = score;
+                HighschorePlayer = playername;
+
+                Console.WriteLine("The new highscore is " + score);
+                Console.WriteLine("The holder of new the highscore is " + playername);
+            }
+            else
+            {
+                Console.WriteLine("your score was " + score);
+                Console.WriteLine("the current Highscore is " + highscore);
+                Console.WriteLine("Current Highscore holder is " + HighschorePlayer);
+            }
+        }
+    }
+}
